0c539586884f3255512b2cbdea3ee51e3c1b0d78
takechanpro4 <takechan45296@yahoo.co.jp>
2020-02-26 19:34:05 +0900

    jidou

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,7 +84,6 @@ $('#new_message').on('submit', function(e){
       contentType: false
     })
      .done(function(data){
-       console.log(data)
        var html = buildHTML(data);
        $('.messages').append(html);      
        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
