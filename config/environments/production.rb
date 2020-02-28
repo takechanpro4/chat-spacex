26f66ce789ef761ab35c757d39f6c94b23ba87b2
takechanpro4 <takechan45296@yahoo.co.jp>
2020-02-28 19:27:26 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+server '52.198.23.138', user: 'ec2-user', roles: %w{app db web}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
