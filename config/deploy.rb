58dbb9a6d269bf319bccaba991d18c5747b90829
takechanpro4 <takechan45296@yahoo.co.jp>
2020-03-02 21:51:02 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/chat-spacex.pem'] 
-
+end
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
@@ -42,7 +42,7 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
+
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
